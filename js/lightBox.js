3c4d4096b8d6242d543c8220b6a0ccc51fa2c255
Elias Pedevilla <eliaspedevilla@Eliass-MacBook-Pro.local>
2022-03-27 19:59:00 -0300

    segundo commit

---
+++ js/lightBox.js
@@ -0,0 +1,25 @@
+const imagenes = document.querySelectorAll('.img-galeria');
+const imagenLight = document.querySelector('.agregar-imagen');
+const contenedorLight = document.querySelector('.imagen-light');
+const close = document.querySelector('.close')
+
+imagenes.forEach(imagen=>{
+    imagen.addEventListener('click', ()=>{
+      aparecerImagen(imagen.getAttribute('src'));
+        
+    })
+})
+
+contenedorLight.addEventListener('click', (e)=>{
+   if(e.target == close) {
+    contenedorLight.classList.toggle('show')
+    imagenLight.classList.toggle('showImage')
+   }
+})
+
+const aparecerImagen= (imagen)=>{
+    imagenLight.src = imagen;
+    contenedorLight.classList.toggle('show')
+    imagenLight.classList.toggle('showImage')
+
+}
